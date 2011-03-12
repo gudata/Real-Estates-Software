dfbbe16f37f8359732fc3ca2b60e14da560b298b
Gudata <i.bardarov@gmail.com>
2011-03-12 22:17:51 +0200

    fix the deploy of database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -163,11 +163,9 @@ namespace :deploy do
 
   # in RAILS_ROOT/config/deploy.rb:
   after 'deploy:update_code', 'deploy:symlink_db'
-  namespace :deploy do
-    desc "Symlinks the database.yml"
-    task :symlink_db, :roles => :app do
-      run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-    end
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   end
 
 end
