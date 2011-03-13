cf8a3638c4211885d6db75534d333f0607765b59
Gudata <i.bardarov@gmail.com>
2011-03-13 19:42:49 +0200

    start a maintain script to check and recover the database from errors.

---
+++ utils/maintain.rb
@@ -0,0 +1,19 @@
+SellDocument.all.each do |sell_document|
+  begin
+    sell_document.sell
+  rescue
+    puts "removing #{document.id}"
+    sell_document.destroy
+  end
+end.reject{true}
+
+Sell.find_each do |sell|
+  begin
+    sell.address
+  rescue
+    sell.address = nil
+    puts "fixing #{sell.id}"
+    sell.save
+    retry
+  end
+end.reject{true}
